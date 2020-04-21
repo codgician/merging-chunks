69f231354e5435da21016826bfac5b601e8d5ace
Shijia Zhang <codgician@users.noreply.github.com>
2020-04-21 23:04:53 +0800

    tests: improve tests

--- src/merger/mod.rs
+++ src/merger/mod.rs
@@ -16,57 +16,66 @@ pub fn merge_chunks(chunk_num: usize, part_exp: usize) -> Result<()> {
     }
 
     let mut chunk_readers: Vec<BufReader<std::fs::File>> = Vec::new();
+    let mut chunk_nexts: Vec<Option<u64>> = Vec::new();
     chunk_readers.reserve_exact(chunk_num);
+    chunk_nexts.reserve_exact(chunk_num);
     for i in 0 .. chunk_num {
         chunk_readers.push(BufReader::new(File::open(i.to_string() + ".in").unwrap()));
+        chunk_nexts.push(read_next(&mut chunk_readers[i]));
     }
 
-    let mut rem: Vec<Option<u64>> = vec![None; chunk_num];
 
     for i in 0 .. (1u64 << part_exp) {
         let lb = (1u64 << (64 - part_exp)) * (i as u64);
         let ub = if i == 0 { (1u64 << (64 - part_exp)) - 1 } else { lb - 1 + (1u64 << (64 - part_exp)) };
 
-        let mut vec: Vec<u64> = Vec::new();
+        let mut v: Vec<u64> = Vec::new();
 
         for j in 0 .. chunk_num {
-            match rem[j] {
-                Some(val) => if val <= ub { vec.push(val); } else { continue; }, 
-                None => (),
+            match chunk_nexts[j] {
+                Some(x) => if x <= ub { v.push(x); } else { continue; }, 
+                None => continue,
             }
 
-            rem[j] = read_chunk(&mut vec, &mut chunk_readers[j], ub);
+            chunk_nexts[j] = read_chunk(&mut v, &mut chunk_readers[j], ub);
         }        
-        sorter::sort(&mut vec);
-        write_result(&vec)?;
+        sorter::sort(&mut v);
+        write_result(&v)?;
     }
 
     Ok(())
 }
 
-fn read_chunk(vec: &mut Vec<u64>, reader: &mut BufReader<std::fs::File>, ub: u64) -> Option<u64> {
+fn read_next(reader: &mut BufReader<std::fs::File>) -> Option<u64> {
+    let mut cur: Vec<u8> = Vec::new();
+
     loop {
-        let mut cur: Vec<u8> = Vec::new();
-        let len = reader.read_until(b' ', &mut cur).expect("read chunk failed");
-    
-        if len == 0 {
-            break;
+        reader.read_until(b' ', &mut cur).expect("read chunk failed");
+
+        match cur.last() {
+            Some(&x) => if x == b' ' { cur.pop(); } else { },
+            None => return None,
         }
 
-        cur.pop();
-        let cur: u64 = from_utf8(&cur).unwrap().parse().expect("not an u64");
-
-        if cur <= ub {
-            vec.push(cur);
-        } else {
-            return Some(cur);
+        if cur.is_empty() {
+            continue;
         }
+
+        return Some(from_utf8(&cur).unwrap().parse().expect("not an u64"))
     }
-
-    None
 }
 
-fn write_result(vec: & Vec<u64>) -> Result<()> {
+fn read_chunk(v: &mut Vec<u64>, reader: &mut BufReader<std::fs::File>, ub: u64) -> Option<u64> {
+    loop {
+        let cur = read_next(reader);
+        match cur {
+            Some(x) => if x <= ub { v.push(x); } else { return Some(x); },
+            None => return None,
+        }
+    }
+}
+
+fn write_result(v: &Vec<u64>) -> Result<()> {
     let file = OpenOptions::new()
                     .write(true)
                     .create(true)
@@ -75,10 +84,11 @@ fn write_result(vec: & Vec<u64>) -> Result<()> {
                     .unwrap();
 
     let mut writer = BufWriter::new(file);
-    for i in vec {
+    for i in v {
         write!(writer, "{} ", i)?;
     }
 
+    writer.flush()?;
     Ok(())
 }
 
@@ -97,44 +107,38 @@ mod tests {
         let mut rng = rand::thread_rng();
         let dist = Uniform::from(lb .. ub);    
 
-        let mut vec = Vec::new(); 
-        vec.reserve_exact(len);
+        let mut v = Vec::new(); 
+        v.reserve_exact(len);
         for _ in 0 .. len {
-            vec.push(dist.sample(&mut rng));
+            v.push(dist.sample(&mut rng));
         }
 
-        vec.sort();
-        vec
+        v.sort();
+        v
     }
 
-    fn write_chunk(id: usize, vec: & Vec<u64>) -> Result<()> {
+    fn write_chunk(id: usize, v: &Vec<u64>) -> Result<()> {
         let file = File::create(id.to_string() + ".in")?;
         let mut writer = BufWriter::new(file);
 
-        for i in vec {
+        for i in v {
             write!(writer, "{} ", i)?;
         }
 
+        writer.flush()?;
         Ok(())
     }
 
-    fn generate_data(num: usize, size: usize) -> Result<()> {
-        for i in 0 .. num {
-            write_chunk(i, & generate_chunk(size, 0, std::u64::MAX))?;
-        }
-    
-        Ok(())
-    }
-
-    fn cleanup_data(chunk_num: usize) -> Result<()> {
+    fn cleanup_test(chunk_num: usize) -> Result<()> {
         for i in 0 .. chunk_num {
             remove_file(i.to_string() + ".in")?;
         }
+        remove_file("result.txt")?;
 
         Ok(())
     }
 
-    fn validate_result() -> bool {
+    fn validate_result(expected: &Vec<u64>) -> bool {
         let file = File::open("result.txt").expect("open result.txt failed");
         let mut reader = BufReader::new(file);
 
@@ -145,8 +149,12 @@ mod tests {
                             .map(|x| x.parse::<u64>().expect("not an u64"))
                             .collect();
 
-        for i in 1 .. v.len() {
-            if v[i - 1] > v[i] {
+        if v.len() != expected.len() {
+            return false;
+        }
+
+        for i in 0 .. v.len() {
+            if v[i] != expected[i] {
                 return false;
             }
         }
@@ -154,15 +162,30 @@ mod tests {
         true
     }
 
-    fn do_test(chunk_num: usize, chunk_size: usize) {
-        generate_data(chunk_num, chunk_size).expect("data generation failed");
+    fn do_test(chunk_num: usize, expected: &Vec<u64>) {
         merge_chunks(chunk_num, 9).expect("merging failed");
-        cleanup_data(chunk_num).expect("cleanup failed");
-        assert_eq!(validate_result(), true);
+        assert_eq!(validate_result(&expected), true);
+        cleanup_test(chunk_num).expect("cleanup failed");
     } 
-
+    
     #[test]
-    fn merger_should_work_for_small_cases() {
-        do_test(1000, 1000);
+    fn should_work_for_small_random_cases() {
+        for _ in 0 .. 10 {
+            const LIM: usize = 100;
+
+            let mut expected: Vec<u64> = Vec::new();
+            expected.reserve(LIM * LIM);
+
+            let mut rng = rand::thread_rng();
+            let dist = Uniform::from(1 .. LIM);    
+            for i in 0 .. LIM {
+                let cur_chunk = generate_chunk(dist.sample(&mut rng), 0, std::u64::MAX);
+                write_chunk(i, &cur_chunk).expect("write chunk failed");
+                expected.extend(&cur_chunk);
+            }
+
+            expected.sort();
+            do_test(LIM, &expected);
+        }
     }
 }
--- src/merger/sorter.rs
+++ src/merger/sorter.rs
@@ -3,45 +3,40 @@ extern crate rayon;
 
 use rayon::prelude::*;
 
-pub fn sort(vec: &mut Vec<u64>) {
-    vec.par_sort_unstable();  
+pub fn sort(v: &mut Vec<u64>) {
+    v.par_sort_unstable();  
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     extern crate rand;
     use rand::distributions::{Distribution, Uniform};
 
     #[test]
-    fn sort_should_work_for_small_case() {
-        let mut v: Vec<u64> = [3, 2, 5, 6, 4, 1].to_vec();
-        sort(&mut v);
-        assert_eq!(v, [1, 2, 3, 4, 5, 6].to_vec());
-    }
-
-    #[test]
-    fn sort_should_work_for_empty() {
+    fn should_work_for_empty() {
         let mut v: Vec<u64> = Vec::new();
         sort(&mut v);
         assert_eq!(v.len(), 0);
     }
 
     #[test]
-    fn sort_should_work_for_large_case() {
+    fn should_work_for_small_cases() {
+        const VEC_SIZE: usize = 100;
+
         let mut rng = rand::thread_rng();
-        let dist = Uniform::from(0 .. std::u64::MAX);
+        let dist = Uniform::from(0 .. std::u64::MAX);    
 
-        let mut v: Vec<u64> = Vec::new();
-        for _ in 0 .. 30000 {
-            v.push(dist.sample(&mut rng));
-        }
-        sort(&mut v);
-
-        for i in 1 .. 30000 {
-            assert_eq!(v[i] >= v[i - 1], true);
+        let mut v: Vec<u64> = vec![0; VEC_SIZE];
+        for _ in 0 .. 10 {
+            for i in 0 .. VEC_SIZE {
+                v[i] = dist.sample(&mut rng);
+            }
+            let mut expected = v.clone();
+            sort(&mut v);
+            expected.sort();
+            assert_eq!(expected, v);
         }
     }
 }
-
